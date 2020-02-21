85148b906b343995f3c0b3cd6bd8fba43a10fb3f
Stardust2019 <46747959+Stardust2019@users.noreply.github.com>
2020-02-20 21:29:55 -0800

    Create sqldatabase.py

---
+++ sqldatabase.py
@@ -0,0 +1,63 @@
+import sqlite3
+import os
+import  sys
+
+sqlQuery = """
+insert into user(`img`,`st_time`,`end_time`) values(%s,%s,%s)
+"""
+class Image(object):
+
+    def __init__(self):
+        self.image_name = []
+
+    def load_directory(self, path='/home/hydro/Pictures'):
+        """
+        :param path: Provide Path of File Directory
+        :return: List of image Names
+        """
+        for x in os.listdir(path):
+            self.image_name.append(x)
+
+        return self.image_name
+
+    def create_database(self, name, image):
+        """
+        :param name: String
+        :param image:  BLOP Data
+        :return: None
+        """
+
+        conn = sqlite3.connect("Imagepi.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS my_table 
+        (name TEXT,image BLOP)""")
+
+        cursor.execute(""" insert into user(`name`,`image`) values(%s,%s)""",(name,image))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
+def main():
+    obj = Image()
+    os.chdir("/home/hydro/Pictures")
+    for x in obj.load_directory():
+
+        if ".png" in x:
+            with open(x,"rb") as f:
+                data = f.read()
+                obj.create_database(name=x, image=data)
+                print("{} Added to database ".format(x))
+
+        elif ".jpg" in x:
+            with open(x,"rb") as f:
+                data = f.read()
+                obj.create_database(name=x,image=data)
+                print("{} added to Database".format(x))
+
+
+if __name__ == "__main__":
+    main()
