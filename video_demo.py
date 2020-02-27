127c737323b4912925e727f660dafde112606fd9
Stardust2019 <46747959+Stardust2019@users.noreply.github.com>
2020-02-26 21:49:50 -0800

    Update video_demo.py

--- video_demo.py
+++ video_demo.py
@@ -425,8 +425,8 @@ def video_processing(graph, category_index, video_file_name, show_video_window, 
                             with open("/home/hydro/person_detection-master/Pictures" + pic_name, 'rb') as f:
                                 dbImage.create_database(name=pic_name, image=f.read())
                                 
-                          if len(person_boxes) == 0 & firstframe_flag == True:
-                            firstframe_flag = False
+                        if len(person_boxes) == 0 & firstframe_flag == True:
+                            #firstframe_flag = False
                             print ("detected at: ")
                             cv2.imwrite('/home/hydro/person_detection-master/Pictures/snapshot_'+str(i)+'.jpg', last_frame)
                             pic_name = "lastframe" + str(frame_counter) + ".jpg"
